92739f227220ada17f912e11184fa8d9f2855cc2
wir0x <gsalazar-v@outlook.com>
2016-01-17 12:59:18 -0400

    first commit

--- AutoLayoutSample/ViewController.swift
+++ AutoLayoutSample/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        logginButton.tintColor = UIColor.whiteColor()
+        logginButton.backgroundColor = UIColor.orangeColor()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
