e010024d368db8e2ac3744028b2d8368aeabf8e4
wir0x <gsalazar-v@outlook.com>
2016-01-17 08:02:34 -0400

    Initial Commit

---
+++ AutoLayoutSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutSample
+//
+//  Created by Gonzalo Salazar Velasquez on 1/17/16.
+//  Copyright © 2016 Gonzalo Salazar Velasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
